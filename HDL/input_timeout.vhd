49df1d0c52b7766a521454c5a7800850d897ed31
sir-drako <67859416+sir-drako@users.noreply.github.com>
2020-08-12 14:50:20 +0200

    Get rid of unnecessary Q output vector for input timeout counter
    Similiar to the key_counter update, the Q vector output port
    is not necessary for synthesizing the VHDL counter
    
    This also eliminates the need for several library packages and the
    log2ceil calculation for figuring out how many bits are needed for
    counting up to a certain delay

--- HDL/input_timeout.vhd
+++ HDL/input_timeout.vhd
@@ -1,49 +1,35 @@
 library ieee;
-use ieee.std_logic_1164.all,
-	ieee.numeric_std.all,
-	ieee.math_real.all;
+use ieee.std_logic_1164.all;
 	
 entity input_timeout is 
-	generic(
-		--constants used so that the max count can be automatically determined for a given delay
-		constant CLOCK_FREQ 	: integer := 100E6; 	--for Quartus II simulation, a 100 MHz clock is used
-		constant DELAY 			: integer := 5			--specified delay in seconds
-	);
-	
 	port(
-		CLK		: in std_logic;
-		RST		: in std_logic;	
-		
-		--(CLOCK_FREQ * DELAY) calculates the number of count cycles necesary for the desired DELAY
-		--the rest of the mathematical expression determines the bit size necessary for this max count using the (log2(n) - 1) technique
-		Q		: buffer unsigned((integer(ceil(log2(real(CLOCK_FREQ * DELAY)))) - 1) downto 0) := (others => '0');
-		DONE 	: out std_logic := '0'
+		clk		: in std_logic;
+		rst		: in std_logic;	
+		done 	: out std_logic := '0'
 	);
 end entity input_timeout;
 
 architecture logic of input_timeout is 
+	constant CLK_FREQ : integer := 100E+6;
+	constant DELAY : integer := 5;
+	constant MAX_CLK_CYCLES : integer := CLK_FREQ*DELAY;
 begin
-	process(CLK,RST) is
+	process(clk,rst)
+		variable tick : integer := 0;
 	begin
-		if RST = '1' then
-			Q <= (others => '0');
-		elsif rising_edge(CLK) then
-			--reset counter if at the end of the count
-			if Q = (CLOCK_FREQ * DELAY) then
-				Q <= (others => '0');			
-			else
-				Q <= Q + 1;
-			end if;
-		end if;
-	end process;
-	
-	process(Q) is
-	begin
-		--signals if 5 seconds has passed
-		if Q = (CLOCK_FREQ * DELAY) then
-			DONE <= '1';
-		else 
-			DONE <= '0';
+		--if the FSM restarts the delay, make sure that the delay done signal is not active
+		if rst = '1' then
+			tick := 0;
+			done <= '0';
+		elsif rising_edge(clk) then
+			tick := tick + 1;
+				if tick = MAX_CLK_CYCLES then
+					done <= '1';
+					--reset tick because otherwise it will continue counting behind MAX_CLK_CYCLES
+					tick := 0;
+				else 
+					done <= '0';
+				end if;
 		end if;
 	end process;
 end architecture logic;
