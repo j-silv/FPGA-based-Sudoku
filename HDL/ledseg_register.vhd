f519db0f39af00b67c4d9e796eed788cc0330b9a
sir-drako <67859416+sir-drako@users.noreply.github.com>
2020-08-12 14:39:44 +0200

    Get rid of output enable signal
    For the current design, the LED segment register is the only device
    that is accessing the RAM data line. There is thus no need for tri-state
    output for the LED segment register
    
    In the future, another bus multiplexer might be implemented because of
    the ROM module which will have to write to the RAM. However, an output
    enable signal is still not necessary for the LED segment register block.

--- HDL/ledseg_register.vhd
+++ HDL/ledseg_register.vhd
@@ -5,38 +5,22 @@ use ieee.std_logic_1164.all;
 
 entity ledseg_register is
 	port(
-		
-		CLK : in std_logic;
-		D : in std_logic_vector(7 downto 0) := x"FF";
-		EN,OE : in std_logic;
-		
-		--tri-state intialized to high-Z
-		Q : out std_logic_vector(7 downto 0) := "ZZZZZZZZ" 
+		CLK, EN : in std_logic;
+		D : in std_logic_vector(7 downto 0);
+		Q : out std_logic_vector(7 downto 0)
 	);
 end entity ledseg_register;
 
 architecture logic of ledseg_register is 
-	signal temp_Q : std_logic_vector(7 downto 0); --storage variable to hold data
 begin 
-
-	--if rising edge detected, we check enable
-	--if enable is on, latch incoming data. If not, ignore change on input
+	--if a clk transition is detected, the enable signal is checked
+	--if EN is on, then latch in the LED segment data
 	latch : process(CLK) is
 	begin
 		if rising_edge(CLK) then
 			if EN='1' then	
-				temp_Q <= D;
+				Q <= D;
 			end if;	
-		end if;
-	end process;
-	
-	--tri state buffer -> if output enable is on, output data
-	data_out : process(temp_Q,OE) is
-	begin
-		if OE='0' then
-			Q <="ZZZZZZZZ";
-		else
-			Q <= temp_Q;
 		end if;
 	end process;
 end architecture logic;
