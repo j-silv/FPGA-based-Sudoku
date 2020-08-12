666925819afd4c963455af66876abdd3749237ce
sir-drako <67859416+sir-drako@users.noreply.github.com>
2020-08-12 14:32:19 +0200

    Update key_counter component by simplifying ports
    At this point, I realized that I didn't need to have a counter that
    had outputs that dictate the current count (pins Q). The count could be
    kept track of with a "tick" variable directly in the process
    
    This simplifies the associated VHDL code

--- HDL/key_counter.vhd
+++ HDL/key_counter.vhd
@@ -1,41 +1,33 @@
 library ieee;
-use ieee.std_logic_1164.all,
-	ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
 	
-entity key_counter is
+entity key_counter is 
 	port(
-		CLK 	: in std_logic; 	--had to use a DFF otherwise the logic was not working - likely a race condition with the d-latch 
-		RST		: in std_logic;
-		
-		Q		: buffer unsigned(1 downto 0) := "00";  --testing using buffer
-		DONE 	: out std_logic := '0'
-	
+		clk		: in std_logic;
+		rst		: in std_logic;	
+		done 	: out std_logic := '0'
 	);
 end entity key_counter;
 
-architecture logic of key_counter is
-	--Counts how many times keyboard input was detected
-	--signal temp_Q : std_logic_vector(1 downto 0):="00"; 			--possible bug if these values are not initially assigned
-	--constant ONE : std_logic_vector(1 downto 0):="01";				--might have to look into quartus documentation to make sure KEY_CNT is 0
-																	--or just have a global reset at the beginning of the program
-																	--> when RESET ='1' then also KEY_RST ='1', DELAY_RST='1' etc.
-
+architecture logic of key_counter is 
+	--the user has to push this many buttons before the data is written to memory
+	constant NUM_BUTTON_PUSHES : integer := 3;
 begin
-	process(CLK, RST) is
+	process(clk,rst)
+		variable tick : integer := 0;
 	begin
-		if RST = '1' then
-			Q <= "00";
-		elsif rising_edge(CLK) then
-			Q <= Q + 1;
-		end if;
-	end process;
-		
-	process(Q) is
-	begin
-		if Q = "11" then
-			DONE <= '1';
-		else 
-			DONE <= '0';
+		--if the counter is reset, then make sure key_done is not an active signal
+		if rst = '1' then
+			tick := 0;
+			done <= '0';
+		elsif rising_edge(clk) then
+			tick := tick + 1;
+				if tick = NUM_BUTTON_PUSHES then
+					--we don't have to reset tick because the FSM will
+					done <= '1';
+				else 
+					done <= '0';
+				end if;
 		end if;
 	end process;
 end architecture logic;
