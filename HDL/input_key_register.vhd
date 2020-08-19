a9a9184cbd4241fcf6dbdc9899958cb2c9d39616
sir-drako <67859416+sir-drako@users.noreply.github.com>
2020-08-19 22:16:01 +0200

    Update input registers with inverse logic levels for new keyboard design

--- HDL/input_key_register.vhd
+++ HDL/input_key_register.vhd
@@ -18,7 +18,7 @@ entity input_key_register is
 		D : in std_logic_vector(9 downto 0);
 		-- bits 9-0 will be for the LED segment data encoder
 		-- bits 8-0 will be for position register 
-		Q : out std_logic_vector(9 downto 0) := "0000000000"		
+		Q : out std_logic_vector(9 downto 0) := "1111111111"		
 	);
 end entity input_key_register;
 
@@ -27,9 +27,12 @@ architecture logic of input_key_register is
 begin
 	process(RST, CLK) is 
 	begin
-		-- asynchronous reset
+		-- asynchronous reset (although this is technically a "set" for the registers)
+		-- the registers are set because when the keyboard is activated, it
+		-- gives a LOW logic signal for the key signals
+		-- when not activated, the key signals are pulled up to a HIGH logic level by internal resistors
 		if RST = '1' then
-			Q <= "0000000000";
+			Q <= "1111111111";
 		elsif rising_edge(CLK) then
 			Q <= D;
 		end if;
--- HDL/input_on_register.vhd
+++ HDL/input_on_register.vhd
@@ -16,7 +16,7 @@ entity input_on_register is
 	port(
 		CLK, RST : in std_logic;
 		D0, D1, D2	: in std_logic;
-		Q0, Q1, Q2	: out std_logic := '0'		
+		Q0, Q1, Q2	: out std_logic := '1'		
 	);
 end entity input_on_register;
 
@@ -25,11 +25,14 @@ architecture logic of input_on_register is
 begin
 	process(RST, CLK) is 
 	begin
-		-- asynchronous reset
+		-- asynchronous reset (although this is technically a "set" for the registers)
+		-- the registers are set because when the keyboard is activated, it
+		-- gives a LOW logic signal for the ON signals
+		-- when not activated, the ON signals are pulled up to a HIGH logic level by internal resistors
 		if RST = '1' then
-			Q0 <= '0';
-			Q1 <= '0';
-			Q2 <= '0';
+			Q0 <= '1';
+			Q1 <= '1';
+			Q2 <= '1';
 		elsif rising_edge(CLK) then
 			Q0 <= D0;
 			Q1 <= D1;
